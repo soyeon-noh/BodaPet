8af7b13daabf4a0e8515887da7d3b09d3e5dca5b
soyeon-noh <1010qwerty12@gmail.com>
2022-06-03 13:20:34 +0900

    yolov5_deepsort 와 node 연결

--- server/routes/mypage.js
+++ server/routes/mypage.js
@@ -16,7 +16,9 @@ router.get("/", async (req, res, next) => {
 
   // 2. spawn을 통해 "python 파이썬파일.py" 명령어 실행
   const result = spawner("python", [
-    "/Users/soyeonnoh/Desktop/2022_workspace/Bodapet/test/test.py",
+    // "/Users/soyeonnoh/Desktop/2022_workspace/Bodapet/test/test.py",
+    // "../test/test.py",
+    "../yolov5_deepsort/yolov5/test.py",
   ]);
   // const result = spawn("python", ["../../test/test.py", "강낭콩"]);
   // 3. stdout의 'data'이벤트리스너로 실행결과를 받는다.
@@ -29,11 +31,11 @@ router.get("/", async (req, res, next) => {
     console.log("pythos code 에러발생", data.toString());
     res.json(data.toString());
   });
-  const 결과 = {
-    success: 1,
-    message: "완료",
-  };
-  res.json(결과);
+  // const 결과 = {
+  //   success: 1,
+  //   message: "완료",
+  // };
+  // res.json(결과);
 });
 
 export default router;
---
+++ yolov5_deepsort/yolov5/test.py
@@ -0,0 +1,4 @@
+# -*- coding: utf8 -*-
+import sys
+
+print('파이썬 파일에 접근했습니다.')
